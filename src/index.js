700dc4b69e85a90647225d5ee23ecf80a8b65fac
Yessid Rios <91134063+YessidR@users.noreply.github.com>
2021-12-17 21:39:44 -0500

    make it better

--- src/index.js
+++ src/index.js
@@ -1,117 +1,95 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
-// import Login from './components/Login.jsx';
-// import DashboardAdmin from './components/administrador/DashboardAdmin.jsx';
-// import DashboardRegistrar from './components/administrador/DashboardRegistrar.jsx';
+import Login from './components/Login.jsx';
+import DashboardAdmin from './components/administrador/DashboardAdmin.jsx';
+import DashboardRegistrar from './components/administrador/DashboardRegistrar.jsx';
 
-// import Inventario from './components/inventario/Inventario.jsx';
-// import IngresarMat from './components/inventario/IngresarMat.jsx'
-// import ConsultarMat from './components/inventario/ConsultarMat.jsx';
-// import EditarMat from './components/inventario/EditarMat.jsx';
-// import ReportesInventario from './components/inventario/ReportesInventario.jsx';
+import Inventario from './components/inventario/Inventario.jsx';
+import IngresarMat from './components/inventario/IngresarMat.jsx'
+import ConsultarMat from './components/inventario/ConsultarMat.jsx';
+import EditarMat from './components/inventario/EditarMat.jsx';
+import ReportesInventario from './components/inventario/ReportesInventario.jsx';
 
-// import Produccion from './components/produccion/Produccion.jsx';
-// import IngresarProd from './components/produccion/IngresarProd.jsx';
-// import CrearOrden from './components/produccion/CrearOrden.jsx';
-// import VerOrden from './components/produccion/VerOrden.jsx';
-// import ReportesProduccion from './components/produccion/ReportesProduccion.jsx';
+import Produccion from './components/produccion/Produccion.jsx';
+import IngresarProd from './components/produccion/IngresarProd.jsx';
+import CrearOrden from './components/produccion/CrearOrden.jsx';
+import VerOrden from './components/produccion/VerOrden.jsx';
+import ReportesProduccion from './components/produccion/ReportesProduccion.jsx';
 
-// import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 
-// import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
-// const tipoUsuario = JSON.parse(localStorage.getItem("tipoUsuario")); 
-
-
-const express = require('express');
-const routerApi = require('./routes');
-
-const app = express();
-const port = 3000;
-
-app.use(express.json()); // Usado como middleware
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
-
-routerApi(app);
-
-app.listen(port, () => {
-  console.log('Mi port ' +  port);
-});
+const tipoUsuario = JSON.parse(localStorage.getItem("tipoUsuario")); 
 
 
-// ---------------------------------------------------------------------------------------------------
+if (tipoUsuario === "administrador") {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Router>
+        <Routes>
 
-// if (tipoUsuario === "administrador") {
-//   ReactDOM.render(
-//     <React.StrictMode>
-//       <Router>
-//         <Routes>
-
-//           <Route path="/" element={<Login/>} />
+          <Route path="/" element={<Login/>} />
           
-//           <Route path="/dashboard" element={<DashboardAdmin />} />
-//           <Route path="/dashboard/registrar" element={<DashboardRegistrar />} />
+          <Route path="/dashboard" element={<DashboardAdmin />} />
+          <Route path="/dashboard/registrar" element={<DashboardRegistrar />} />
             
-//           <Route path="/inventario" element={<Inventario/>} />
-//           <Route path="/inventario/ingresar" element={<IngresarMat/>} />
-//           <Route path="/inventario/consultar" element={<ConsultarMat/>} />
-//           <Route path="/inventario/editar" element={<EditarMat/>} />
-//           <Route path="/inventario/reportes" element={<ReportesInventario/>} />
+          <Route path="/inventario" element={<Inventario/>} />
+          <Route path="/inventario/ingresar" element={<IngresarMat/>} />
+          <Route path="/inventario/consultar" element={<ConsultarMat/>} />
+          <Route path="/inventario/editar" element={<EditarMat/>} />
+          <Route path="/inventario/reportes" element={<ReportesInventario/>} />
           
-//           <Route path="/produccion" element={<Produccion/>} />
-//           <Route path="/produccion/crear" element={<IngresarProd/>} />
-//           <Route path="/produccion/orden" element={<CrearOrden/>} />
-//           <Route path="/produccion/listado" element={<VerOrden/>} />
-//           <Route path="/produccion/reportes" element={<ReportesProduccion/>} />
+          <Route path="/produccion" element={<Produccion/>} />
+          <Route path="/produccion/crear" element={<IngresarProd/>} />
+          <Route path="/produccion/orden" element={<CrearOrden/>} />
+          <Route path="/produccion/listado" element={<VerOrden/>} />
+          <Route path="/produccion/reportes" element={<ReportesProduccion/>} />
           
-//         </Routes>
-//       </Router>
-//     </React.StrictMode>,
-//     document.getElementById('root')
-//   ) 
-// } else if (tipoUsuario === "inventario") {
-//   ReactDOM.render(
-//     <React.StrictMode>
-//       <Router>
-//         <Routes>
-//           <Route path="/" element={<Login/>} />
-//           <Route path="/inventario" element={<Inventario/>} />
-//           <Route path="/inventario/ingresar" element={<IngresarMat/>} />
-//           <Route path="/inventario/consultar" element={<ConsultarMat/>} />
+        </Routes>
+      </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
+  ) 
+} else if (tipoUsuario === "inventario") {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login/>} />
+          <Route path="/inventario" element={<Inventario/>} />
+          <Route path="/inventario/ingresar" element={<IngresarMat/>} />
+          <Route path="/inventario/consultar" element={<ConsultarMat/>} />
           
-//           <Route path="/inventario/editar" element={<EditarMat/>} />
-//           <Route path="/inventario/reportes" element={<ReportesInventario/>} />
-//         </Routes>
-//       </Router>
-//     </React.StrictMode>,
-//     document.getElementById('root')
-//   )
-// } else if (tipoUsuario === "produccion") {
-//   ReactDOM.render(
-//     <React.StrictMode>
-//       <Router>
-//         <Routes>         
-//           <Route path="/" element={<Login/>} />
-//           <Route path="/produccion" element={<Produccion/>} />
-//           <Route path="/produccion/crear" element={<IngresarProd/>} />
-//           <Route path="/produccion/orden" element={<CrearOrden/>} />
-//           <Route path="/produccion/listado" element={<VerOrden/>} />
-//           <Route path="/produccion/reportes" element={<ReportesProduccion/>} />
-//         </Routes>
-//       </Router>
-//     </React.StrictMode>,
-//     document.getElementById('root')
-//   )
-// } 
+          <Route path="/inventario/editar" element={<EditarMat/>} />
+          <Route path="/inventario/reportes" element={<ReportesInventario/>} />
+        </Routes>
+      </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
+  )
+} else if (tipoUsuario === "produccion") {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Router>
+        <Routes>         
+          <Route path="/" element={<Login/>} />
+          <Route path="/produccion" element={<Produccion/>} />
+          <Route path="/produccion/crear" element={<IngresarProd/>} />
+          <Route path="/produccion/orden" element={<CrearOrden/>} />
+          <Route path="/produccion/listado" element={<VerOrden/>} />
+          <Route path="/produccion/reportes" element={<ReportesProduccion/>} />
+        </Routes>
+      </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
+  )
+} 
 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
